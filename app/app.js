9ba05c680b544efbd0255073d8c8ccdfdb312b33
Quinault <aquinault@gmail.com>
2015-08-13 15:35:25 +0000

    maj

--- app/app.js
+++ app/app.js
@@ -1,22 +1,9 @@
-/*import React from "react";
-import Greeting from "./greeting";
-
-import 'bootstrap/dist/css/bootstrap.css';
-
-React.render(
-  <Greeting name="World"/>,
-  document.getElementById('app')
-);
-*/
-
-
 var React = window.React = require('react'),
     Timer = require("./ui/Timer"),
     mountNode = document.getElementById("app"),
     Chart1 = require("./ui/Chart1");
 
 import 'bootstrap/dist/css/bootstrap.css';
-import 'url?limit=10000!./logo.png';
 
 
 var Router = require('react-router');
@@ -34,32 +21,11 @@ var routes = (
 var RouteHandler = Router.RouteHandler;
 
 
-var TodoList = React.createClass({
-  render: function() {
-    var createItem = function(itemText) {
-      return <li>{itemText}</li>;
-    };
-    return <ul>{this.props.items.map(createItem)}</ul>;
-  }
-});
 var TodoApp = React.createClass({
-  getInitialState: function() {
-    return {items: [], text: ''};
-  },
-  onChange: function(e) {
-    this.setState({text: e.target.value});
-  },
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var nextItems = this.state.items.concat([this.state.text]);
-    var nextText = '';
-    this.setState({items: nextItems, text: nextText});
-  },
   render: function() {
     return (
       <div>
         <RouteHandler/>
-
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,6 @@ module.exports = {
         { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
         { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
         { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },      
-        //{ test: /\.(png|jpg)$/, loader: 'url?limit=8192'}, 
       ],
     },
     devServer: {
